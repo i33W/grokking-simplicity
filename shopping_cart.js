0c5b293f1da132c53401f81abd8d19a379a931ca
i33W <hwangbomee@kakao.com>
2022-07-07 16:06:51 +0900

    무료 배송비 계산을 위한 새로운 코드 추가

--- shopping_cart.js
+++ shopping_cart.js
@@ -16,4 +16,19 @@ function calc_cart_total() {
     shopping_cart_total += item.price;
   }
   set_cart_total_dom();
+  update_shipping_icons();
+}
+
+// 무료 배송비 계산하기
+function update_shipping_icons() {
+  var buy_buttons = get_buy_buttons_dom();
+  for (var i = 0; i < buy_buttons.length; i++) {
+    var button = buy_buttons[i];
+    var item = button.item;
+    if (item.price + shopping_cart_total >= 20) {
+      button.show_free_shipping_icon();
+    } else {
+      button.hide_free_shipping_icon();
+    }
+  }
 }
